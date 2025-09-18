95fefdeb51de5929984f5e83f0e65fde8d81889d
Ute <ute@Users-iMac-2.lan>
2025-09-17 22:17:14 -0700

    Fix ScrollToTop path -- ../components/ScrollToTop

--- src/utils/cn.js
+++ src/utils/cn.js
@@ -1,6 +1,6 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-import ScrollToTop from './components/ScrollToTop';
+import ScrollToTop from '../components/ScrollToTop';
 
 export function cn(...inputs) {
     return twMerge(clsx(inputs));
