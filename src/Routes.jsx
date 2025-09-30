9403d56970a8ac7b78706ca101bdd3f07a3b73e6
sentAIent <sales@sentaient.com>
2025-09-29 18:08:55 -0700

    Routes.jsx -> update iTravel import & path

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -9,6 +9,7 @@ import HomepageAIConsultancyHub from './pages/homepage-ai-consultancy-hub';
 import AISolutionsExperienceCenter from './pages/ai-solutions-experience-center';
 import TrustTransparencyHub from './pages/trust-transparency-hub';
 import AboutOurApproachIntelligenceCenter from './pages/about-our-approach-intelligence-center';
+import iTravel from './pages/iTravel';
 
 const Routes = () => {
   return (
@@ -24,6 +25,7 @@ const Routes = () => {
         <Route path="/ai-solutions-experience-center" element={<AISolutionsExperienceCenter />} />
         <Route path="/trust-transparency-hub" element={<TrustTransparencyHub />} />
         <Route path="/about-our-approach-intelligence-center" element={<AboutOurApproachIntelligenceCenter />} />
+        <Route path="/iTravel" element={<iTravel />} />
         <Route path="*" element={<NotFound />} />
       </RouterRoutes>
       </ErrorBoundary>
