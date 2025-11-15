30269a2683e7bd2acbde8c7f48d5229d4b269f93
ute <ute@stljp.net>
2025-11-14 16:52:06 -0800

    Update team expertise

--- src/pages/about-our-approach-intelligence-center/components/HeroSection.jsx
+++ src/pages/about-our-approach-intelligence-center/components/HeroSection.jsx
@@ -9,7 +9,7 @@ const HeroSection = () => {
       <div className="hero-content">
         <h1 className="block text-3xl lg:text-4xl font-bold">
           Amplifying
-          <span className="block text-foreground mt-8">Human Achievement</span>
+          <span className="block text-accent mt-8">Human Achievement</span>
           {/* <span className="block text-accent">Achievement</span> */}
           <span className="block text-white">With AI</span>
         </h1>
