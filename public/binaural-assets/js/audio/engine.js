f53255e5c0f2d355ff66692cbd87be8f2bd7d635
Brian <bl.nb.ca@gmail.com>
2026-02-05 13:15:35 -0800

    Upgrade to Visualizer Nuclear v4 and update core modules

--- public/binaural-assets/js/audio/engine.js
+++ public/binaural-assets/js/audio/engine.js
@@ -1,5 +1,5 @@
 import { state, els, SOUNDSCAPES, STATE_INSIGHTS, SOUND_INSIGHTS } from '../state.js';
-import { getVisualizer, initVisualizer } from '../visuals/visualizer_nuclear_v3.js';
+import { getVisualizer, initVisualizer } from '../visuals/visualizer_nuclear_v4.js';
 import { stopRecording } from '../export/recorder.js';
 import { DailyLimitService } from '../services/daily-limit.js';
 import { showPricingModal } from '../ui/pricing-3tier.js';
@@ -877,7 +877,7 @@ export function updateFrequencies() {
         if (targetSpeed > 40.0) targetSpeed = 40.0;
 
         // Update Visualizer
-        import('../visuals/visualizer_nuclear_v3.js').then(m => {
+        import('../visuals/visualizer_nuclear_v4.js').then(m => {
             const viz = m.getVisualizer();
             if (viz) viz.setSpeed(targetSpeed);
         });
