5d188bef2df64a747aaa9c0d77474b021c85a484
Brian <bl.nb.ca@gmail.com>
2026-07-17 21:11:00 -0700

    fix: add HologramGuide stub to fix build

---
+++ src/pages/demo3d/components/HologramGuide.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HologramGuide = ({ appId, position }) => {
+  // Stubbed component to prevent build failures.
+  // TODO: Replace with the actual HologramGuide implementation.
+  return (
+    <group position={position}>
+    </group>
+  );
+};
+
+export default HologramGuide;
